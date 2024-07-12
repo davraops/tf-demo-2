acadfab3f87c8615819103c8c4da141d953c4e7a
Rafael Avella <114040665+davraops@users.noreply.github.com>
2024-07-12 15:22:42 +0000

    fix

--- nonprod/main.tf
+++ nonprod/main.tf
@@ -96,6 +96,31 @@ resource "aws_secretsmanager_secret_version" "db_credentials_version" {
 
 # DB
 
+resource "aws_security_group" "db_sg" {
+  name        = "db-security-group"
+  description = "Security group for RDS instance"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "db-security-group"
+  }
+}
+
+
 resource "aws_db_instance" "default" {
   allocated_storage    = 20
   storage_type         = "gp2"
